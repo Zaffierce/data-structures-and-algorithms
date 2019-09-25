aa1691a88e08bacdef70e34568bdfc5b96d6afa7
Jon Veach <veach09@gmail.com>
2019-09-24 22:14:37 -0700

    Linked List Insertion methods!

--- code401challenges/src/main/java/code401challenges/LinkededList/LinkedList.java
+++ code401challenges/src/main/java/code401challenges/LinkededList/LinkedList.java
@@ -38,41 +38,41 @@ public class LinkedList {
         return result;
     }
 
-    public static void main(String[] args) {
-        LinkedList link_before = new LinkedList();
-//        link.head = new Node(1);
-//        link.insert(3);
-//        link.insert(10);
-//        link.insert(2);
-//        link.insert(1);
-        link_before.insert(3);
-        link_before.insert(2);
-        link_before.insert(1);
-        link_before.insertBefore(3,10);
-        System.out.println(link_before.toString());
-
-        LinkedList link_after = new LinkedList();
-        link_after.insert(3);
-        link_after.insert(10);
-        link_after.insert(2);
-        link_after.insert(1);
-        System.out.println(link_after.toString());
-//        link.insertBefore(3, 10);
-
-
-//        link.head = new Node(1); //Head
-//        link.insert(2); //Third, or fourth if insertBefore
-//        link.insert(4); //Second
-//        link.append(12); //This adds 12 to the end.
-//        link.insert(8); //First
-//        link.insertBefore(2, 88); //Third
-//        link.insertAfter(2, 99);
-////        System.out.println(link.includes(10)); //This returns false as 10 is not in the linked list.
-////        System.out.println(link.includes(8)); //This returns true as 8 is in our list.
+//    public static void main(String[] args) {
+//        LinkedList link_before = new LinkedList();
+////        link.head = new Node(1);
+////        link.insert(3);
+////        link.insert(10);
+////        link.insert(2);
+////        link.insert(1);
+//        link_before.insert(3);
+//        link_before.insert(2);
+//        link_before.insert(1);
+//        link_before.insertBefore(3,10);
+//        System.out.println(link_before.toString());
+//
+//        LinkedList link_after = new LinkedList();
+//        link_after.insert(3);
+//        link_after.insert(10);
+//        link_after.insert(2);
+//        link_after.insert(1);
+//        System.out.println(link_after.toString());
+////        link.insertBefore(3, 10);
 //
 //
-
-    }
+////        link.head = new Node(1); //Head
+////        link.insert(2); //Third, or fourth if insertBefore
+////        link.insert(4); //Second
+////        link.append(12); //This adds 12 to the end.
+////        link.insert(8); //First
+////        link.insertBefore(2, 88); //Third
+////        link.insertAfter(2, 99);
+//////        System.out.println(link.includes(10)); //This returns false as 10 is not in the linked list.
+//////        System.out.println(link.includes(8)); //This returns true as 8 is in our list.
+////
+////
+//
+//    }
 
     public String toString() {
         Node n = head;
@@ -117,6 +117,26 @@ public class LinkedList {
             }
             current = current.next;
         }
+    }
+
+    public int kthFromEnd(int k) {
+        int llSizeCounter = 0;
+        int result = 0;
+        Node currNode = this.head;
+
+        while(currNode != null) {
+            currNode = currNode.next;
+            llSizeCounter++;
+        }
+        llSizeCounter = llSizeCounter - k;
+        currNode = this.head;
+        int stepper = 0;
+        while (stepper < k) {
+            stepper++;
+            currNode = currNode.next;
+            result = currNode.data;
+        }
+        return result;
     }
 
 }
--- code401challenges/src/main/java/code401challenges/LinkededList/README.md
+++ code401challenges/src/main/java/code401challenges/LinkededList/README.md
@@ -5,3 +5,8 @@ Create a LinkedList that can be able to insert new values, check for duplicates 
  * link.insert(int) - Inserts an integer into the LinkedList.
  * link.includes(int) - Checks the LinkedList to see if this value exists or not.
  * link.toString() - Prints out the entire LinkedList.
+ * link.append(int) - Appends an integer to your LinkedList.
+ * link.insertBefore(int insertValue, int newValue) - Inserts into LinkedList before your insertValue.  I.e. insertBefore(3, 88) would insert before the item with the value of 3 in your LL.
+ * link.insertAfter(int insertValue, int newValue) - Inserts into LinkedList after your insertValue.  I.e. insertAfter(4, 99) would insert after the item with the value of 4 in your LL.
+ * link.kthFromEnd(int k) - Returns the value k positions from the end, based on 0 index.  I.e. kthFromEnd(2) where your LL is [1][2][3][4][5]  would return 3.
+ 
